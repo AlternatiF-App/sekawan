352543dbe08e4ea9b0746c82c9cceaa679ef319b
Mohamad Agung Faisal <agoenkf97@gmail.com>
2023-11-10 20:07:58 +0700

    add responsive web

--- app/restaurant/[id]/page.tsx
+++ app/restaurant/[id]/page.tsx
@@ -48,7 +48,7 @@ const DetailPage = () => {
 
   return (
     <div>
-      <div className='w-full h-[500px] relative'>
+      <div className='w-full md:h-[500px] h-[250px] relative'>
         <Image
           src={data[0]?.photo.images.original.url}
           className='w-full h-full object-cover'
@@ -57,21 +57,21 @@ const DetailPage = () => {
           width={1280}
         />
         
-        <div className='absolute w-full h-[500px] bg-black/60 top-0 text-white'>
-          <nav className='container mx-auto w-10/12 py-4 px-6'>
-            <div className='flex space-x-8 items-center'>
-              <div className='flex items-center'>
-                <AiFillPhone className='h-5 w-5 mr-2' />
+        <div className='absolute w-full md:h-[500px] h-[250px] bg-black/60 top-0 text-white'>
+          <nav className='container mx-auto w-10/12 py-4 md:px-6 px-4'>
+            <div className='md:flex md:space-x-12 justify-between text-xs md:text-base'>
+              <div className='flex items-center py-1'>
+                <AiFillPhone className='md:h-5 h-3 md:w-5 w-3 mr-2' />
                 { data[0]?.phone }
               </div>
               <div className='flex items-center'>
-                <FaLocationDot className='h-5 w-5 mr-2' />
+                <FaLocationDot className='md:h-5 h-3 md:w-5 w-3 mr-2' />
                 { data[0]?.address }
               </div>
             </div>
-            <div className='flex flex-col py-20 items-center justify-center space-y-4'>
-              <h2 className='text-2xl'>Welcome To</h2>
-              <h1 className='font-bold text-6xl' >{ data[0]?.name}</h1>
+            <div className='flex flex-col md:py-20 py-4 items-center justify-center md:space-y-4 space-y-2'>
+              <h2 className='md:text-2xl text-base'>Welcome To</h2>
+              <h1 className='font-bold md:text-6xl text-xl text-center' >{ data[0]?.name}</h1>
               <div className='flex items-center space-x-4'>
                 <Star rate={parseInt(data[0]?.rating)} page='detail' />
                 <span className='block pt-1'>{ data[0]?.rating } ({ data[0]?.num_reviews }) </span>
@@ -84,10 +84,12 @@ const DetailPage = () => {
         </div>
       </div>
 
-      <div className='container mx-auto w-10/12 px-6 py-4'>
-        <div className='flex items-center'>
-          <BiFoodMenu className='w-5 h-5 text-blue-950 mr-2' />
-          <span className='block mr-2'>Cuisine : </span>
+      <div className='container mx-auto md:w-10/12 px-6 py-4'>
+        <div className='flex flex-wrap md:flex-nowrap items-center md:text-lg text-sm '>
+          <div>
+            <BiFoodMenu className='w-5 h-5 text-blue-950 md:mr-2' />
+          </div>
+          <div className='block mr-2 '>Cuisine : </div>
           {
             data[0]?.cuisine.map((item: any, index: number) => {
               return <span key={index} className='block' >{ item.name }{ data[0]?.cuisine.length !== index + 1 && ', ' }</span>
@@ -96,18 +98,18 @@ const DetailPage = () => {
         </div>
 
         <div className='mt-6 font-extralight'>
-          <span className='block text-2xl'>Description</span>
-          <p className='text-justify'>{ data[0]?.description !== '' ? data[0]?.description : 'Tidak ada deskripsi' }</p>
+          <span className='block md:text-2xl text-xl'>Description</span>
+          <p className='text-justify md:text-lg text-sm'>{ data[0]?.description !== '' ? data[0]?.description : 'Tidak ada deskripsi' }</p>
         </div>
 
         <div className='mt-6 font-extralight'>
-          <span className='block text-2xl'>Review</span>
-          <div className='w-full overflow-x-auto flex space-x-6 py-4'>
+          <span className='block md:text-2xl text-xl'>Review</span>
+          <div className='w-full overflow-x-auto flex md:space-x-6 space-x-2 md:py-4 py-2'>
             {
               reviews?.map((review: any, index: number) => (
-                <div key={index} className='rounded-md px-4 py-4 border border-blue-950/20 shadow-md'>
-                  <div className='flex items-center space-x-4'>
-                    <div className='w-12 h-12'>
+                <div key={index} className='rounded-md md:px-4 py-4 border border-blue-950/20 shadow-lg'>
+                  <div className='flex items-center space-x-4 px-4'>
+                    <div className='md:w-12 w-6 md:h-12 h-6'>
                       <Image
                         className='w-full h-full object-cover rounded-full'
                         src={review?.user.avatar.small.url}
@@ -117,11 +119,11 @@ const DetailPage = () => {
                       />
                     </div>
                     <div>
-                      <h1>{ review.user.username }</h1>
+                      <h1 className='md:text-lg text-base'>{ review.user.username }</h1>
                     </div>
                   </div>
 
-                  <div className='w-[300px] py-4'>
+                  <div className='w-[300px] py-4 md:text-lg text-sm px-4'>
                     <span className='font-bold flex items-center'>
                       <AiOutlineComment className='h-5 w-5 mr-2' />
                       { review.title }
